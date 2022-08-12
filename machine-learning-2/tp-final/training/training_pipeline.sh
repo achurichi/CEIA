94d5c531198136c3a8a58c4c15a77ece23480cbb
Alan Churichi <achurichi@gmail.com>
2022-08-12 01:11:57 -0300

    Rename

--- machine-learning-2/tp-final/training/training_pipeline.sh
+++ machine-learning-2/tp-final/training/training_pipeline.sh
@@ -1,4 +1,4 @@
-PGPASSWORD=<password> psql -h <public-ip> -d postgres -U postgres -c "\copy employee_attrition TO 'data.csv' WITH (FORMAT CSV, HEADER)"
+PGPASSWORD=<db-password> psql -h <db-public-ip> -d postgres -U postgres -c "\copy employee_attrition TO 'data.csv' WITH (FORMAT CSV, HEADER)"
 python train_model.py
 latest=$(ls -t mlruns/0/ | head -1)
 gsutil cp -r mlruns/0/$latest/artifacts/model gs://ml2-model-bucket/production
