2432e8607e263c7e099f95b4639979a545f33150
Alan Churichi <achurichi@gmail.com>
2022-08-12 01:10:59 -0300

    Cloud function now stores prediction in db

--- machine-learning-2/tp-final/cloud-functions/external-inference/main.py
+++ machine-learning-2/tp-final/cloud-functions/external-inference/main.py
@@ -1,11 +1,27 @@
 import requests
+from sqlalchemy import create_engine
+
 
 def predict(external_request):
-    headers = {
-        'Content-Type': 'application/json',
-        'format': 'pandas-records'
-    }
+    external_request = external_request.json
+    user_id = external_request["user_id"]
+    data = external_request["data"]
 
-    response = requests.post('http://34.102.116.204:5000/invocations', headers=headers, json=external_request.json)
+    response = requests.post(
+        "http://<serving-vm-public-ip>:5000/invocations",
+        headers={"Content-Type": "application/json"},
+        json=data,
+    )
+
+    engine = create_engine(
+        "postgresql+psycopg2://postgres:<db-password>@<db-public-ip>:5432/postgres"
+    )
+    connection = engine.connect()
+    for entry, pred in zip(data, response.json()):
+        keys = ", ".join(str(key) for key in entry.keys())
+        values = ", ".join(str(value) for value in entry.values())
+        connection.execute(
+            f"INSERT INTO public.predictions (user_id, {keys}, prediction) VALUES ('{user_id}', {values}, {str(pred)})"
+        )
 
     return response.text
