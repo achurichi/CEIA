7120b6f0325eb828653591a9e0fdb313ea27286a
Alan Churichi <achurichi@gmail.com>
2022-08-13 00:52:11 -0300

    Add model registry

--- machine-learning-2/tp-final/training/train_model.py
+++ machine-learning-2/tp-final/training/train_model.py
@@ -1,28 +1,52 @@
 import pandas as pd
 import mlflow
+from mlflow.tracking import MlflowClient
 from imblearn.over_sampling import RandomOverSampler
 from sklearn.linear_model import LogisticRegression
 from preprocessing import split, preprocess_dataset
-from utils import report_scores
+from utils import upload_files, report_scores
 from constants import RANDOM_STATE
+
+connection_string = (
+    "postgresql+psycopg2://postgres:<db-password>@<db-public-ip>:5432/postgres"
+)
+
+mlflow.set_tracking_uri(connection_string)
+mlflow.set_registry_uri(connection_string)
+client = MlflowClient()
 
 # Cargar Dataset
 df = pd.read_csv("data.csv")
-# df = pd.read_csv("/tf/notebooks/CEIA/machine-learning-2/tp-final/training/data.csv")
 df = preprocess_dataset(df)
 
-# Entrenamiento del modelo
+# Separacion en train y test
 X_train, X_test, y_train, y_test = split(df)
-
 oversampler = RandomOverSampler(sampling_strategy="minority", random_state=RANDOM_STATE)
 X_train, y_train = oversampler.fit_resample(X_train, y_train)
 
-model = LogisticRegression(class_weight="balanced")
-mlflow.start_run()
-model.fit(X_train, y_train)
-mlflow.sklearn.log_model(model, "model")
+# Entrenamiento del modelo
+with mlflow.start_run() as run:
+    model = LogisticRegression(class_weight="balanced")
+    model.fit(X_train, y_train)
+    mlflow.sklearn.log_model(model, "model")
+    run_id = run.info.run_id
 
+result = mlflow.register_model(
+    f"mlruns/0/{run_id}/artifacts/model", "sk-learn-logistic-regression"
+)
+
+# Testing
 y_pred = model.predict(X_test)
 y_proba = model.predict_proba(X_test)
-
 report_scores(y_test, y_pred, y_proba)
+
+# Promote
+client.transition_model_version_stage(
+    name="sk-learn-logistic-regression",
+    version=result.version,
+    stage="Production",
+    archive_existing_versions=True,
+)
+
+# Guardar el modelo
+upload_files("ml2-model-bucket", f"mlruns/0/{run_id}/artifacts/model")
--- machine-learning-2/tp-final/training/utils.py
+++ machine-learning-2/tp-final/training/utils.py
@@ -1,9 +1,36 @@
+import os
+from pathlib import Path
+from google.cloud import storage
 from sklearn.metrics import (
     balanced_accuracy_score,
     roc_auc_score,
     precision_score,
     recall_score,
 )
+
+
+def upload_files(bucket_name, folder_name):
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    for path, _, files in os.walk(folder_name):
+        for name in files:
+            path_local = os.path.join(path, name)
+            blob_path = path_local.replace("\\", "/")
+            blob = bucket.blob(blob_path)
+            blob.upload_from_filename(path_local)
+
+
+def download_files(bucket_name, bucket_dir):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blobs = bucket.list_blobs(prefix=bucket_dir)
+    for blob in blobs:
+        if blob.name.endswith("/"):
+            continue
+        file_split = blob.name.split("/")
+        directory = "/".join(file_split[0:-1])
+        Path(directory).mkdir(parents=True, exist_ok=True)
+        blob.download_to_filename(blob.name)
 
 
 def report_scores(y_test, y_pred, y_proba):
