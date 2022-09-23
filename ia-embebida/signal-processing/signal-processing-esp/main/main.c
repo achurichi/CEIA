eb3b41becd4f2200b467ced1cb97d266c284400b
Alan Churichi <achurichi@gmail.com>
2022-09-22 23:50:29 -0300

    Use moving average as second filter

--- ia-embebida/signal-processing/signal-processing-esp/main/main.c
+++ ia-embebida/signal-processing/signal-processing-esp/main/main.c
@@ -5,57 +5,33 @@
 #include "driver/uart.h"
 #include "soc/uart_reg.h"
 #include "soc/uart_struct.h"
-#include "../../cfunctions.c"
 
-#define WINDOW_SIZE 4
-
-double x_coef[] = {0.00031579, 0.00094736, 0.00094736, 0.00031579};
-
-double y_coef[] = {1., -2.71506105, 2.46928626, -0.75169892};
-
-// double x_coef[] = {
-//     6.85369982e-09,
-//     4.79758987e-08,
-//     1.43927696e-07,
-//     2.39879494e-07,
-//     2.39879494e-07,
-//     1.43927696e-07,
-//     4.79758987e-08,
-//     6.85369982e-09,
-// };
-
-// double y_coef[] = {
-//     1.,
-//     -6.35957907,
-//     17.36022673,
-//     -26.36667776,
-//     24.06161832,
-//     -13.19284752,
-//     4.02392261,
-//     -0.52666242,
-// };
+#define WINDOW_SIZE 20
 
 static intr_handle_t handle_console;
 
-uint32_t in[WINDOW_SIZE] = {0, 0, 0, 0, 0, 0, 0, 0};
-uint32_t out[WINDOW_SIZE] = {0, 0, 0, 0, 0, 0, 0, 0};
+uint64_t sum;
+uint16_t elementCount;
+double mean;
+uint32_t circularBuffer[WINDOW_SIZE];
+uint32_t *circularBufferAccessor = circularBuffer;
 
-uint32_t filterSample(uint32_t sample)
+void appendToBuffer(uint32_t value)
 {
-  for (int i = WINDOW_SIZE - 1; i >= 1; i--) // Se rotan las entradas
-    in[i] = in[i - 1];
-  in[0] = sample; // La nueva lectura se almacena en la posición 0
+  *circularBufferAccessor = value;
+  circularBufferAccessor++;
+  if (circularBufferAccessor >= circularBuffer + WINDOW_SIZE)
+    circularBufferAccessor = circularBuffer;
+}
 
-  double output = (double)in[0] * x_coef[0];
-
-  for (int i = 1; i < WINDOW_SIZE; i++) // Se calcula la salida
-    output += (double)in[i] * x_coef[i] - (double)out[i - 1] * y_coef[i - 1];
-  for (int i = WINDOW_SIZE - 1; i >= 1; i--) // Se rotan las salidas
-    out[i] = out[i - 1];
-
-  out[0] = (uint32_t)(output > 0 ? output : 0);
-
-  return out[0];
+double simpleMovingAverage(int value)
+{
+  sum -= *circularBufferAccessor;
+  sum += value;
+  appendToBuffer(value);
+  if (elementCount < WINDOW_SIZE)
+    ++elementCount;
+  return (double)sum / elementCount;
 }
 
 static void IRAM_ATTR
@@ -74,7 +50,7 @@ uart_intr_handle(void *arg)
                       ((uint32_t)data[1] << 8) |
                       data[0];
 
-    uint32_t result = filterSample(sample);
+    uint32_t result = (uint32_t)simpleMovingAverage(sample);
 
     uart_write_bytes(UART_NUM_0, &result, sizeof(uint32_t));
   }
