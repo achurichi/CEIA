c97faf8382cc0265b5b6e198b643a076cd99b28e
Alan Churichi <achurichi@gmail.com>
2022-10-05 02:42:53 -0300

    Working classifier

--- ia-embebida/tp-final/tp-final-esp/main/main.c
+++ ia-embebida/tp-final/tp-final-esp/main/main.c
@@ -6,53 +6,53 @@
 #include "soc/uart_reg.h"
 #include "soc/uart_struct.h"
 
-#define WINDOW_SIZE 20
-
 static intr_handle_t handle_console;
 
-uint64_t sum;
-uint16_t elementCount;
-double mean;
-uint32_t circularBuffer[WINDOW_SIZE];
-uint32_t *circularBufferAccessor = circularBuffer;
-
-void appendToBuffer(uint32_t value)
+double predict(uint8_t *data)
 {
-  *circularBufferAccessor = value;
-  circularBufferAccessor++;
-  if (circularBufferAccessor >= circularBuffer + WINDOW_SIZE)
-    circularBufferAccessor = circularBuffer;
-}
+  uint8_t zeroCrossing = data[0];
+  uint8_t maximum = data[1];
+  uint8_t simetry = data[2];
 
-double simpleMovingAverage(int value)
-{
-  sum -= *circularBufferAccessor;
-  sum += value;
-  appendToBuffer(value);
-  if (elementCount < WINDOW_SIZE)
-    ++elementCount;
-  return (double)sum / elementCount;
+  if (maximum <= 50.5)
+  {
+    if (maximum <= 39.5)
+    {
+      return 1;
+    }
+    else
+    {
+      return simetry <= 95.5 ? 0 : 1;
+    }
+  }
+  else
+  {
+    if (simetry <= 127.5)
+    {
+      return zeroCrossing <= 4.5 ? 0 : 2;
+    }
+    else
+    {
+      return simetry <= 214.5 ? 2 : 0;
+    }
+  }
 }
 
 static void IRAM_ATTR
 uart_intr_handle(void *arg)
 {
   uint8_t len = UART0.status.rxfifo_cnt;
-  uint8_t data[4];
-  if (len >= 4)
+  uint8_t data[3];
+  if (len >= 3)
   {
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 3; i++)
     {
       data[i] = UART0.fifo.rw_byte;
     }
-    uint32_t sample = ((uint32_t)data[3] << 24) |
-                      ((uint32_t)data[2] << 16) |
-                      ((uint32_t)data[1] << 8) |
-                      data[0];
 
-    uint32_t result = (uint32_t)simpleMovingAverage(sample);
+    uint8_t result = predict(data);
 
-    uart_write_bytes(UART_NUM_0, &result, sizeof(uint32_t));
+    uart_write_bytes(UART_NUM_0, &result, sizeof(uint8_t));
   }
   uart_flush(UART_NUM_0);
   uart_clear_intr_status(UART_NUM_0, UART_RXFIFO_FULL_INT_CLR | UART_RXFIFO_TOUT_INT_CLR);
